2feb99e779757047d4e4d9526db9269f8ac58214
Crimix <christofferm@live.dk>
2019-06-22 13:50:38 +0200

    - Last little thing

--- src/main/java/com/black_dog20/mininglantern/reference/Reference.java
+++ src/main/java/com/black_dog20/mininglantern/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String MOD_NAME = "Mining Lantern";
 	public static final String VERSION = "@VERSION@";
 	public static final String MC_VERSIONS = "[1.12,1.12.2]";
-	public static final String DEPENDENCIES = "";//"required-after:albedo@[0.1.2,)";
+	public static final String DEPENDENCIES = "after:baubles";//"required-after:albedo@[0.1.2,)";
 	public static final String CLIENT_PROXY_CLASS = "com.black_dog20.mininglantern.proxies.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.black_dog20.mininglantern.proxies.ServerProxy";
 }
